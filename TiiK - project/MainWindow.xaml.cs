dc3ba84ce6a381492f4a3aa3f8763c57962fa9cd
Filip Pietraszak <filippiet@gmail.com>
2019-10-29 20:35:19 +0100

    First gui iteration

--- TiiK - project/MainWindow.xaml.cs
+++ TiiK - project/MainWindow.xaml.cs
@@ -20,9 +20,46 @@ namespace TiiK___project
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
+        }
+
+        private void button_OpenFileDialog_Click(object sender, RoutedEventArgs e) {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "TXT Files (*.txt)|*txt";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true) {
+                string filename = dlg.FileName;
+                textBox_FileLocation.Text = filename;
+            }
+        }
+
+        private void button_Analyze_Click(object sender, RoutedEventArgs e) {
+            Console.WriteLine();
+            var data = new List<AnalyzedData>();
+
+            //
+            var sample = new AnalyzedData { Character = 'a', Count = 13, Probability = 0.13, Quantity = 3.14 };
+            data.Add(sample);
+            sample = new AnalyzedData { Character = 'b', Count = 55, Probability = 0.63, Quantity = 8.14 };
+            data.Add(sample);
+            sample = new AnalyzedData { Character = 'c', Count = 73, Probability = 0.23, Quantity = 1.11 };
+            data.Add(sample);
+            //
+
+            //var data = countQuantities(textBox_FileLocation.Text);
+
+            dataGrid_AnalyzeResults.ItemsSource = data;
+        }
+
+        private struct AnalyzedData
+        {
+            public char Character { set; get; }
+            public double Count { set; get; }
+            public double Probability { set; get; }
+            public double Quantity { set; get; }
         }
     }
 }
